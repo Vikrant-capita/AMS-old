207ddbe5e93b67664e0d1266f0c3b5218645860a
Vikrant-capita <117144840+Vikrant-capita@users.noreply.github.com>
2023-01-04 13:24:59 +0530

    Update DateConversionFormat.java

--- src/main/java/utils/DateConversionFormat.java
+++ src/main/java/utils/DateConversionFormat.java
@@ -136,6 +136,37 @@ public class DateConversionFormat {
 	}
 
 	
+	//================================convert "Month Text"(-DEC-) into int.==========================
+	public ArrayList<Integer> validateMonthText(String datetext) {
+		ArrayList<Integer> exceptionLeavePending=new ArrayList<Integer>();
+		
+		System.out.println("Schedule date : "+datetext); 
+		int date = Integer.parseInt(datetext.split("-")[0]);
+		int year = Integer.parseInt(datetext.split("-")[2]);
+		String Month = datetext.split("-")[1];
+
+		HashMap<String,Integer> hm = new HashMap<String,Integer>();
+		hm.put("Jan", 1);
+		hm.put("Feb", 2);
+		hm.put("Mar", 3);
+		hm.put("Apr", 4);
+		hm.put("May", 5);
+		hm.put("Jun", 6);
+		hm.put("Jul", 7);
+		hm.put("Aug", 8);
+		hm.put("Sep", 9);
+		hm.put("Oct", 10);
+		hm.put("Nov", 11);
+		hm.put("Dec", 12);
+
+		int month = hm.get(Month);
+		exceptionLeavePending.add(date);
+		exceptionLeavePending.add(month);
+		exceptionLeavePending.add(year);
+		return exceptionLeavePending;
+	}
+
+	
 	
 	
 }
